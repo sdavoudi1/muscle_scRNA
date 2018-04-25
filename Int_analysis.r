fcf18744e4cc3a1b961ff1dd36857521551e647e
Sadegh Davoudi <sadegh.davoudi@mail.utoronto.ca>
2018-04-25 13:29:45 -0400

    Created the file for Integrated analysis of young and aged muscle scRNA analysis

---
+++ Int_analysis.r
@@ -0,0 +1,8 @@
+# Int_analysis.R
+# Integrated Analysis of young and aged muscle scRNA
+
+# This scripts takes the young and aged muscle scRNA data and combines them using the Seurat algorithm
+# outlined in Butler et al, Nat Biotech, 2018.
+
+# --------------------------------------------------------------------------------------------------------
+
